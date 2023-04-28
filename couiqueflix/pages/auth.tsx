23818f50eed8dae797a65e93df6643d0541b6835
octavieploye <octavie.ploye@gmail.com>
2023-04-29 00:21:52 +0200

    toggle login/register LOGIC SET

--- couiqueflix/pages/auth.tsx
+++ couiqueflix/pages/auth.tsx
@@ -1,6 +1,8 @@
+
 import Input from "@/components/input"
 import Image from "next/image"
 import { useState } from "react"
+import { useCallback } from "react"
 
 
 // Auth page - Sign In Input setup
@@ -8,6 +10,13 @@ const Auth = () => {
     const [email, setEmail] = useState('')
     const [name, setName] = useState('')
     const [password, setPassword] = useState('')
+
+    // WE  CREATE A TOGGLE BETWEEN LOGIN AND THE REGISTER HERE
+    const [variant, setVariant] = useState('login')
+    // WE USE THE USECALLBACK HOOK TO APPLY THE LOGIC IN THE LOGIN/REGISTER EVENTS
+    const toggleVariant = useCallback(() => {
+        setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login')
+    },[])
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -22,20 +31,25 @@ const Auth = () => {
                          />
 
                     </nav>
+                        {/* SIGN IN SECTION */}
                     <div className="flex justify-center">
-                        {/* SIGN IN BUTTON */}
+                        {/* SIGN IN BOX */}
                         <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2  lg:w-2/5 lg:max-w-md rounded-md w-full ">
 
                             <h2 className="text-white text-4xl mb-8 font-semibold">
-                                Sign In
+                                {/* SIGNIN LOGIC */}
+                                {variant === 'login' ? 'Sign In' : 'Register'}
                             </h2>
                             <div className="flex flex-col gap-4">
-                                <Input
-                                label='Username'
-                                onChange={(ev: any) => setName(ev.target.value)}
-                                id="Username"
-                                value={name}
-                                />
+                                {/*LOGIC FOR REGISTER - (SHOWS THE USERNAME INPUT VS LOGIN)  */}
+                                {variant === 'register' && (
+                                    <Input
+                                    label='Username'
+                                    onChange={(ev: any) => setName(ev.target.value)}
+                                    id="Username"
+                                    value={name}
+                                    />
+                                )}
                                 <Input
                                 label='Email'
                                 onChange={(ev: any) => setEmail(ev.target.value)}
@@ -48,9 +62,27 @@ const Auth = () => {
                                 onChange={(ev: any) => setPassword(ev.target.value)}
                                 id="password"
                                 type="password"
-                                value={email}
+                                value={password}
                                 />
                             </div>
+                            {/* LOGIN / REGISTER BUTTON */}
+                            <button className="bg-red-600 py-3  text-white rounded-md w-full mt-10 hover-bg-red-700 transition ">
+                                {/* TOGGLE BUTTON LOGIN OR REGISTER LOGIC */}
+                                {variant === 'login' ? 'Login' : 'Sign Up'}
+                            </button>
+                            <p className="text-neutral-500 mt-12">
+                                
+                                {/* TOGGLE LOGIN OR REGISTER  LOGIC */}
+                                {variant === 'login' ? "First Time using CouiqueFlix?" : "Already have an account?"}
+                                <span 
+                                onClick={toggleVariant}
+                                className="text-white ml-1 hover:underline cursor-pointer">
+                                    {/* TOGGLE LOGIN OR CREATE AN ACCOUNT */}
+                                    {variant === 'login' ? 'Create an Account' : 'Login'}
+                                </span>
+
+
+                            </p>
                         </div>
 
                     </div>
