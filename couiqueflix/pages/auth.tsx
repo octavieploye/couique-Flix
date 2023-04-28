57fbb28becb0bd7d3db5eec0b48ff72c08531d7b
octavieploye <octavie.ploye@gmail.com>
2023-04-29 00:31:45 +0200

    auth color fix

--- couiqueflix/pages/auth.tsx
+++ couiqueflix/pages/auth.tsx
@@ -20,7 +20,7 @@ const Auth = () => {
 
     return (
         <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-                <div className=" w-full h-full lg:bg-opacity-50">
+                <div className=" bg-black w-full h-full lg:bg-opacity-50">
                     <nav className="px-12 py-5">
                         <Image 
                         src="/images/logo.png" 
