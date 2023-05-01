123acf49d2b7efbe9baff32ece078c3dd74ed902
octavieploye <octavie.ploye@gmail.com>
2023-05-01 13:03:53 +0200

    register return a 200 added

--- couiqueflix/pages/api/register.ts
+++ couiqueflix/pages/api/register.ts
@@ -36,6 +36,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             }
         })
 
+        return res.status(200).json(user)
+
     } catch (error) {
         console.log(error);
         return res.status(400).end()
