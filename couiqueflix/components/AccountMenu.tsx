2c8e33bea2cc2cd55822b067db63cf2ef75074f0
octavieploye <octavie.ploye@gmail.com>
2023-05-06 15:58:01 +0200

    accountMenu signout fix

--- couiqueflix/components/AccountMenu.tsx
+++ couiqueflix/components/AccountMenu.tsx
@@ -21,7 +21,7 @@ const AccountMenu: React.FC<AccountMenuProps> =({visible})=> {
                 <hr className="bg-gray-600 border-0 h-px my-4" />
                 
                 <div 
-                onCLick={()=>signOut()}
+                onClick={() => signOut()}
                 className='px-3 text-center text-white text-sm hover:underline'>
                 Sign Out of Couiqueflix
                 </div>
