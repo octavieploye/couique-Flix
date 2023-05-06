3c6299666868ce16e0f9473b3d11c46f04856287
octavieploye <octavie.ploye@gmail.com>
2023-05-06 23:51:02 +0200

    favorite POST & DELETE methods

---
+++ couiqueflix/pages/api/favorite.ts
@@ -0,0 +1,88 @@
+
+// * THIS FILE CONTAINS THE METHODS "POST" & "DELETE" FOR THE FAVORITE API
+import { NextApiRequest, NextApiResponse } from 'next'
+import { without } from 'lodash'
+
+import prismadb from '@/database/prismadb';
+import serverAuth from '@/database/serverAuth';
+
+// * SET POST & DELETE REQUESTS METHODS FOR THE FAVORITE API
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    try {
+        // HANDLER FOR POST REQUESTS
+        if(req.method === "POST") {
+            // Check if the user is authenticated
+            const { currentUser } = await serverAuth(req,res) ;
+            // Get the movieId from the request body
+            const { movieId } = req.body
+            // FIND THE MOVIE IN THE DATABASE
+            const existingMovie = await prismadb.movie.findUnique({
+                where: {
+                    id: movieId
+                }
+            })
+            // IF THE MOVIE DOESN'T EXIST, THROW AN ERROR
+            if(!existingMovie) {
+                throw new Error('Movie not found')
+            }
+            // UPDATE THE USER'S FAVORITES
+            const user = await prismadb.user.update({
+                where: {
+                    email: currentUser.email || '',
+        },
+            // ADD THE MOVIE TO THE USER'S FAVORITES(FAVORITEIDS -SEE PRISMA SCHEMA-)
+                data: {
+                    favoriteIds: {
+                        // PUSH THE MOVIE ID TO THE FAVORITEIDS ARRAY
+                        push: movieId
+                    }
+                }
+            })
+            // RETURN THE UPDATED USER
+            return res.status(200).json(user)
+        }
+        // HANDLER FOR DELETE REQUESTS
+        if(req.method === "DELETE") {
+            // Check if the user is authenticated
+            const { currentUser } = await serverAuth(req,res) ;
+            // Get the movieId from the request body
+            const { movieId } = req.body
+            // FIND THE MOVIE IN THE DATABASE
+            const existingMovie = await prismadb.movie.findUnique({
+                where: {
+                    id: movieId
+                }
+            })
+            // IF THE MOVIE DOESN'T EXIST, THROW AN ERROR
+            if(!existingMovie) {
+                throw new Error('Movie not found')
+            }
+            // FILTER THE FAVORITEIDS ARRAY TO REMOVE THE MOVIE ID
+            // const updatedFavoriteIds = without(currentUser.favoriteIds, movieId)
+
+            // UPDATE THE USER'S FAVORITES
+            const updatedUser = await prismadb.user.update({
+                where: {
+                    email: currentUser.email || '',
+                },
+                // REMOVE THE MOVIE FROM THE USER'S FAVORITES(FAVORITEIDS -SEE PRISMA SCHEMA-)
+                data: {
+                    favoriteIds: {
+                        // FILTER THE FAVORITEIDS ARRAY TO REMOVE THE MOVIE ID
+                        set: without(currentUser.favoriteIds, movieId)
+                    }
+                }
+            })
+            // RETURN THE UPDATED USER
+            return res.status(200).json(updatedUser)
+        }
+        // IF THE REQUEST IS NEITHER A POST OR DELETE REQUEST, THROW AN ERROR
+        return res.status(405).end()
+        // ERROR HANDLING
+    } catch (error) {
+        console.log(error);
+        return res.status(400).end()
+        
+    }
+
+}
