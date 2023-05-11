33264c304fcd15cef557e45acc0a94ed3150a4da
octavieploye <octavie.ploye@gmail.com>
2023-05-11 17:35:15 +0200

    cached removed

--- couiqueflix/database/fetcher.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-// Fetch data from a URL
-const fetcher = (url: string) => axios
-    .get(url)
-    .then(res => res.data);
-
-    export default fetcher;
--- couiqueflix/database/prismadb.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-//** We save prismaClient in a globalglobal.d.ts  file that is not affected by nextAuth loading  multiple instances of PrismaClient*/
-
-const client = global.prismadb || new PrismaClient()
-if(process.env.NODE_ENV === 'production') global.prismadb = client
-
-export default client
